78b9136f96e5b549beaa8c9d4bfd0ddab8614cbb
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-11-26 16:07:16 -0500

    Added model movie

---
+++ showtime/src/main/java/co/edu/poli/showtime/model/Movie.java
@@ -0,0 +1,11 @@
+package co.edu.poli.showtime.model;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+    private Long id;
+    private String title;
+    private String director;
+    private int rating;
+}

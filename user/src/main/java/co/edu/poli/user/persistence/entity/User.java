d47914699b94f2984e8a6d1a30824e08fe5a135b
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-11-26 15:19:46 -0500

    Added entity and persistence for user

---
+++ user/src/main/java/co/edu/poli/user/persistence/entity/User.java
@@ -0,0 +1,39 @@
+package co.edu.poli.user.persistence.entity;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

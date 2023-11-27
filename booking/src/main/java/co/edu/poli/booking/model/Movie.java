142dde91d5e2711afac458a22b36439d8e2a7b9f
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-11-26 20:19:55 -0500

    added movie model

---
+++ booking/src/main/java/co/edu/poli/booking/model/Movie.java
@@ -0,0 +1,13 @@
+package co.edu.poli.booking.model;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+    private Long id;
+    private String title;
+
+    private String director;
+
+    private int rating;
+}

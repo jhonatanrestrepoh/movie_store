6b292fcbb87a649a9bdb1a3e3f4786ce059dbdfa
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-11-29 16:07:15 -0500

    Added testSaveUser

---
+++ user/src/test/java/co/edu/poli/user/UserRepositoryMockTest.java
@@ -0,0 +1,36 @@
+package co.edu.poli.user;
+
+import co.edu.poli.user.persistence.entity.User;
+import co.edu.poli.user.persistence.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class UserRepositoryMockTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testSaveUser() {
+        // Crear un nuevo usuario
+        User user = new User();
+        user.setName("John");
+        user.setLastName("Doe");
+
+        // Guardar el usuario en la base de datos
+        userRepository.save(user);
+
+        // Recuperar el usuario de la base de datos por ID
+        User savedUser = userRepository.findById(user.getId()).orElse(null);
+
+        // Verificar que el usuario se haya guardado correctamente
+        assertNotNull(savedUser);
+        assertEquals(user.getName(), savedUser.getName());
+        assertEquals(user.getLastName(), savedUser.getLastName());
+    }
+}

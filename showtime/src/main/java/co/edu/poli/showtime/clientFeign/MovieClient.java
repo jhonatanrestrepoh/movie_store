ae4f08c87bbb8b87407c406185e35419075a361d
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-12-03 15:09:35 -0500

    added movie client

--- showtime/src/main/java/co/edu/poli/showtime/clientFeign/MovieClient.java
+++ showtime/src/main/java/co/edu/poli/showtime/clientFeign/MovieClient.java
@@ -1,14 +1,17 @@
 package co.edu.poli.showtime.clientFeign;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import co.edu.poli.common.response.Response;
 import org.springframework.web.bind.annotation.PathVariable;
-import response.Response;
+
 
 @FeignClient(name = "movie")
 public interface MovieClient {
 
     @GetMapping("/api/v1/poli/movie/{id}")
-    Response findById(@PathVariable Long id);
+    Response findById(@PathVariable("id") Long id);
 
 }

d67595d1732e7c267146d9bff0b8c029ad18e8d2
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-12-03 13:54:29 -0500

    added scan packages

--- movie/src/main/java/co/edu/poli/movie/MovieApplication.java
+++ movie/src/main/java/co/edu/poli/movie/MovieApplication.java
@@ -3,7 +3,7 @@ package co.edu.poli.movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "co.edu.poli")
 public class MovieApplication {
 
 	public static void main(String[] args) {

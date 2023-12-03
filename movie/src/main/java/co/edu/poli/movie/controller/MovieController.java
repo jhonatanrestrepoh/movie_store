86047315565df2ebe5b5cf5c4012a33176648e11
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-12-03 15:07:03 -0500

    Added format response

--- movie/src/main/java/co/edu/poli/movie/controller/MovieController.java
+++ movie/src/main/java/co/edu/poli/movie/controller/MovieController.java
@@ -2,21 +2,18 @@ package co.edu.poli.movie.controller;
 
 import co.edu.poli.common.response.Response;
 import co.edu.poli.common.response.ResponseBuild;
+import co.edu.poli.common.response.FormatResponse;
+
 import co.edu.poli.movie.dto.MovieDTO;
-import co.edu.poli.movie.persistence.entity.Movie;
+
 import co.edu.poli.movie.service.MovieService;
 import jakarta.validation.Valid;
-import jakarta.ws.rs.NotFoundException;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/movie")
@@ -25,12 +22,13 @@ public class MovieController {
 
     private final MovieService movieService;
     private final ResponseBuild build;
+    private final FormatResponse formatResponse;
 
     @PostMapping
     public Response save(@Valid @RequestBody MovieDTO movieDTO, BindingResult result) {
 
         if (result.hasErrors()) {
-            return build.failed(format(result));
+            return build.failed(formatResponse.format(result));
         }
 
         movieService.save(movieDTO);
@@ -61,12 +59,4 @@ public class MovieController {
         return build.success(movieService.findAll());
     }
 
-    private List<Map<String, String>> format(BindingResult result) {
-        List<Map<String, String>> errors = result.getFieldErrors().stream().map(error -> {
-            Map<String, String> err = new HashMap<>();
-            err.put(error.getField(), error.getDefaultMessage());
-            return err;
-        }).collect(Collectors.toList());
-        return errors;
-    }
 }

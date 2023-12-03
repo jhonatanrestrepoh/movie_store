e25ed66e38d41a550ff1fc0e73814e53855ca1c9
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-12-03 13:55:54 -0500

    added validations in dto

--- movie/src/main/java/co/edu/poli/movie/dto/MovieDTO.java
+++ movie/src/main/java/co/edu/poli/movie/dto/MovieDTO.java
@@ -1,13 +1,20 @@
 package co.edu.poli.movie.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class MovieDTO {
 
+    @NotEmpty(message = "The title can not be empty")
     private String title;
 
+    @NotEmpty(message = "The director can not be empty")
     private String director;
 
+    @Min(value = 1, message = "The rating must be equal o greater that 1")
+    @Max(value = 5, message = "The rating must be equal o smaller that 5")
     private int rating;
 }
